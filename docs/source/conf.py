f04d591f167ca0654b658091390bf23de0267da9
Andrey <andrey@genestack.com>
2015-02-06 16:45:12 +0400

    Add path to repo into docs config.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,8 +10,9 @@
 #
 # All configuration values have a default; values that are commented out
 # serve to show the default.
+import sys
 
-import sys, os
+sys.path.insert(0, '../../')  # Use pythonSDK form current repository
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
