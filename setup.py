a3c2a2da1bff36cdd3e28b4fa4130f42db8b3370
Andrey <andrey@genestack.com>
2015-06-16 11:02:36 +0300

    Fix ssl warnings.

--- setup.py
+++ setup.py
@@ -22,5 +22,5 @@ setup(
     author_email='',
     description='Genestack API',
     scripts=['genestack-user-setup.py', 'genestack-application-manager.py', 'genestack-uploader.py'],
-    install_requires=['keyring', 'requests']
+    install_requires=['keyring', 'requests[security]']
 )
