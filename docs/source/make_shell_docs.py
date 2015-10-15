4abc199d5577b76ca47569a8532f0630c5c54d9d
andrey <andrey@genestack.com>
2015-10-15 17:53:22 +0300

    Cleanup

--- docs/source/make_shell_docs.py
+++ docs/source/make_shell_docs.py
@@ -81,7 +81,7 @@ def generate_rst_doc(shell_name, class_name, footer_file_name, save_path):
 
 
 def main():
-    generate_rst_doc('genestack-application-manager', 'ApplicationManager', 'app-manager_header.txt', os.path.join('scripts', 'genestack-application-manager.rst'))
+    generate_rst_doc('genestack-application-manager', 'ApplicationManager', 'app-manager_footer.txt', os.path.join('scripts', 'genestack-application-manager.rst'))
     generate_rst_doc('genestack-shell', 'Shell', None, os.path.join('scripts', 'genestack-shell.rst'))
     generate_rst_doc('genestack-user-setup', 'UserManagement', None, os.path.join('scripts', 'genestack-user-setup.rst'))
 
