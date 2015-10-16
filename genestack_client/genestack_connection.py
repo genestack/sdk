9a8adbe4130e9a51d585b3a020caae1ae747177b
andrey <andrey@genestack.com>
2015-10-16 16:29:56 +0300

    Add update command example to warning print. fixes #5117

--- genestack_client/genestack_connection.py
+++ genestack_client/genestack_connection.py
@@ -107,10 +107,15 @@ class Connection:
             return ''
 
         compatible = StrictVersion(version_map[COMPATIBLE])
+
+        update_message = ('You can update it with next command:\n'
+                          '    pip install git+https://github.com/genestack/python-client@stable\n')
+
         if my_verison >= compatible:
-            return 'Newer version "%s" available, please update.' % latest_version
+            return 'Newer version "%s" available, please update.\n%s' % (latest_version, update_message)
         else:
-            raise GenestackException('Your version "%s" is too old, please update to %s' % (my_verison, latest_version))
+            raise GenestackException('Your version "%s" is too old, please update to %s.\n%s' % (
+                my_verison, latest_version, update_message))
 
     def logout(self):
         """
