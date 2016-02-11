2d4919a83d0fdf5210ce33200cf65a1ad15dd1bb
Andrey <andrey@genestack.com>
2016-02-11 16:16:31 +0300

    Print empty line on shell exit

--- genestack_client/genestack_shell.py
+++ genestack_client/genestack_shell.py
@@ -377,5 +377,6 @@ class GenestackShell(cmd.Cmd):
         # TODO on ctrl+C don't exit
         try:
             cmd.Cmd.cmdloop(self, intro=intro)
-        except KeyboardInterrupt:
+        except (KeyboardInterrupt, EOFError):
+            print
             self.postloop()
