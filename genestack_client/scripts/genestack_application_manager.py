970b3ab3b7f04b41be7e0bb36bfab79d21b48eaf
Eugene <eugene@genestack.com>
2016-03-23 15:41:44 +0400

    Fixed output type

--- genestack_client/scripts/genestack_application_manager.py
+++ genestack_client/scripts/genestack_application_manager.py
@@ -166,7 +166,7 @@ class ListVersions(Command):
             stable_versions = self.connection.application(APPLICATION_ID).invoke('getStableVersions', app_id)
         result = self.connection.application(APPLICATION_ID).invoke('listVersions', app_id, self.args.show_owned)
         if not result:
-            print 'No versions found'
+            sys.stderr.write('No versions found')
             return
         result.sort()
 
