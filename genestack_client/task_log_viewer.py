91bf8f5380dde9331474d39c32ed4cb8044d7b0a
Daniel Ohayon <daniel@genestack.com>
2016-04-05 10:46:33 +0100

    add comments + method to get long logs

--- genestack_client/task_log_viewer.py
+++ genestack_client/task_log_viewer.py
@@ -31,6 +31,10 @@ class TaskLogViewer(Application):
         ``follow=True`` will wait until initialization is finished.
         Incoming logs will be printed to the console.
 
+        NB: if the log is longer than 128,000 characters, only the last chunk will be retrieved.
+        If you want to retrieve the full log of a finished task,
+        use :py:meth:`~genestack_client.TaskLogViewer.view_long_log` instead.
+
         :param accession: file accession
         :param log_type: `stdout` or `stderr`
         :param follow: if enabled, wait and display new lines as they appear (similar to ``tail --follow``)
@@ -43,22 +47,59 @@ class TaskLogViewer(Application):
         while True:
             log_chunk = self.invoke('getFileInitializationLog', accession, log_type, offset, limit)
             if not log_chunk:
-                raise GenestackException('File %s not found or have no tasks' % accession)
+                raise GenestackException('File %s not found or has no initialization task' % accession)
 
             if log_chunk['content'] is None and not follow:
+                # there are no logs and we don't wait for them
                 break
             elif log_chunk['content'] is None and log_chunk['isTerminal']:
+                # there are no logs and the task is finished
                 print 'This log is empty (perhaps there was no log produced)'
                 break
             elif log_chunk['content'] is None and not log_chunk['isTerminal']:
+                # there are no logs and the task is not finished
                 if follow:
                     time.sleep(0.5)
                 else:
                     print 'No log produced yet...'
                     break
             elif log_chunk['content'] is not None:
+                # there are logs to show
                 sys.stdout.write(log_chunk['content'])
                 sys.stdout.flush()
+
             if log_chunk['isTerminal'] or not follow:
                 break
+
             offset = log_chunk['offset']
+
+    def view_long_log(self, accession, log_type=None):
+        """
+        Print the full initialization log of a file to `stdout` (even if the log is over 128,000 characters).
+        This method only works for files whose initialization task is finished.
+
+        :param accession: file accession
+        :param log_type: `stdout` or `stderr`
+        """
+
+        if not log_type:
+            log_type = self.STDOUT
+        offset = 0
+        limit = 128000
+        result = ""
+        i = 1
+        while True:
+            print "Retrieving chunk #%d..." % i
+            log_chunk = self.invoke('getFileInitializationLog', accession, log_type, offset, limit)
+            if not log_chunk:
+                raise GenestackException('File %s not found or has no initialization task' % accession)
+            if not log_chunk['isTerminal']:
+                raise GenestackException("Cannot retrieve long log for task in progress")
+            if log_chunk['content'] is None:
+                break
+            result += log_chunk['content']
+            offset += limit
+            i += 1
+        print result
+
+
