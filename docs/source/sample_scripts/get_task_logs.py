60cac1b14c16092376b41e7e92301e42ef832b5b
Daniel Ohayon <daniel@genestack.com>
2016-07-13 14:51:13 +0100

    use constants instead of stderr/stdout

--- docs/source/sample_scripts/get_task_logs.py
+++ docs/source/sample_scripts/get_task_logs.py
@@ -6,8 +6,11 @@ from genestack_client import TaskLogViewer, make_connection_parser, get_connecti
 # add extra arguments to the Genestack arguments parser for this script
 parser = make_connection_parser()
 parser.add_argument('-f', '--follow', action='store_true', help="Follow the logs' output if the task is not done")
-parser.add_argument('-t', '--type', metavar='<log_type>', choices=['stderr', 'stdout'], default='stdout',
-                    help="Type of logs to display ('stdout' or 'stderr' ; default is 'stdout')")
+parser.add_argument('-t', '--type', metavar='<log_type>',
+                    choices=[TaskLogViewer.STDERR, TaskLogViewer.STDOUT],
+                    default=TaskLogViewer.STDOUT,
+                    help="Type of logs to display ('{0}' or '{1}' ; default is '{0}')".format(
+                        TaskLogViewer.STDOUT, TaskLogViewer.STDERR))
 parser.add_argument('accession', metavar='<accession>', help='Accession of the file for which to display the logs')
 arguments = parser.parse_args()
 
