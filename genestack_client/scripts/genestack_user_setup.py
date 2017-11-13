7377a857962f97dc15b215b75d450df9c8a4b212
Andrey Solomatin <andrey@genestack.com>
2017-11-13 15:43:45 +0300

    Code formatting

--- genestack_client/scripts/genestack_user_setup.py
+++ genestack_client/scripts/genestack_user_setup.py
@@ -25,7 +25,7 @@ def validate_alias(alias):
 
 def input_alias(existing):
     print ('Please input alias. (Alias can contain: letters (a-Z), '
-          'digit (0-9), at (@), underscore (_), hyphen (-))')
+           'digit (0-9), at (@), underscore (_), hyphen (-))')
     while True:
         alias = raw_input('alias: ').strip()
         if not alias:
