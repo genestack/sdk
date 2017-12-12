f55f2e4d3f1c7dbc39d493511c0aef7a75277205
Andrey Solomatin <andrey@genestack.com>
2017-12-12 12:42:47 +0300

    Remove temp branch used for testing II

--- genestack_client/genestack_exceptions.py
+++ genestack_client/genestack_exceptions.py
@@ -78,7 +78,7 @@ class GenestackVersionException(GenestackException):
         :type required_version: distutils.version.StrictVersion
         """
         if required_version:
-            branch = 'inception' if required_version.prerelease else 'stable'
+            branch = 'master' if required_version.prerelease else 'stable'
             message = (
                 'Your Genestack Client version "{current_version}" is too old, '
                 'at least "{required_version}" is required.\n'
