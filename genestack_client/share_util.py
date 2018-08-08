9d7b6cdf602b3bbb9d4a7df4b1bcc13171abeeff
Nikolay Shuyskiy <klas@genestack.com>
2018-08-08 12:52:50 +0300

    share_util: re-indent docstring

--- genestack_client/share_util.py
+++ genestack_client/share_util.py
@@ -49,9 +49,10 @@ class ShareUtil(Application):
         :param group_accession: accession of the group to share the files with
         :type group_accession: str
         :param destination_folder: accession of the folder to link shared files into. Typically
-               this parameter should be used for linking files into group folders, which is
-               currently impossible to do using the :meth:`FilesUtil.link_file` method. No links
-               will be created if this parameter is equal to `None`.
+                                   this parameter should be used for linking files into group
+                                   folders, which is currently impossible to do using the
+                                   :meth:`FilesUtil.link_file` method. No links will be created if
+                                   this parameter is equal to `None`.
         :type destination_folder: str
         """
         self.__share(file_accessions, group_accession, destination_folder, 'shareFilesForViewing')
