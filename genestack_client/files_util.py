91c5484c855e8d8a47efd1ad30c17af48dc5c30d
Nadezhda Ratskevich <nadezhda.ratskevich@genestack.com>
2018-09-20 12:42:13 +0300

    update docs

--- genestack_client/files_util.py
+++ genestack_client/files_util.py
@@ -270,7 +270,7 @@ class FilesUtil(Application):
         Add a string value to the metainfo of specified files.
 
         :param accession_list: list of files to be updated
-        :type accession_list: list
+        :type accession_list: list[str]
         :param key: metainfo key
         :type key: str
         :param value: string
@@ -284,7 +284,7 @@ class FilesUtil(Application):
         Replace a string value in the metainfo of specified files.
 
         :param accession_list: list of files to be updated
-        :type accession_list: list
+        :type accession_list: list[str]
         :param key: metainfo key
         :type key: str
         :param value: string
@@ -298,7 +298,7 @@ class FilesUtil(Application):
         Replace a value in the metainfo of specified files.
 
         :param accession_list: list of files to be updated
-        :type accession_list: list
+        :type accession_list: list[str]
         :param key: metainfo key
         :type key: str
         :param value: metainfo value
@@ -312,7 +312,7 @@ class FilesUtil(Application):
         Delete a key from the metainfo of specified files.
 
         :param accession_list: list of files to be updated
-        :type accession_list: list
+        :type accession_list: list[str]
         :param key: metainfo key
         :type key: str
         :rtype: None
