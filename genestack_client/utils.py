5811991e54140802b73fafab3cfc3e9a1f7908b9
Vasiliy Bout <vasiliy@genestack.com>
2019-06-05 09:50:28 +0300

    connection_parser: code style cleanup

--- genestack_client/utils.py
+++ genestack_client/utils.py
@@ -64,13 +64,17 @@ def make_connection_parser(user=None, password=None, host=None, token=None):
     """
     parser = GenestackArgumentParser()
     group = parser.add_argument_group('connection')
-    group.add_argument('-H', '--host', default=host, help="server host", metavar='<host>')
-    group.add_argument('-u', '--user', dest='user', metavar='<user>', default=user, help='user alias from settings or email')
-    group.add_argument('-p', '--password', dest='pwd', default=password, metavar='<password>', help='user password')
+    group.add_argument('-H', '--host', default=host, help='server host', metavar='<host>')
+    group.add_argument('-u', '--user', dest='user', metavar='<user>', default=user,
+                       help='user alias from settings or email')
+    group.add_argument('-p', '--password', dest='pwd', default=password, metavar='<password>',
+                       help='user password')
     group.add_argument('--token', dest='token', default=token, metavar='<api-token>',
                        help='API token to be used instead of the login and password')
-    group.add_argument('--debug', dest='debug', help='print additional stacktrace on error', action='store_true')
-    group.add_argument('--show-logs', dest='show_logs', help="print application logs (received from server)", action='store_true')
+    group.add_argument('--debug', dest='debug', action='store_true',
+                       help='print additional stacktrace on error')
+    group.add_argument('--show-logs', dest='show_logs', action='store_true',
+                       help='print application logs (received from server)')
     return parser
 
 
