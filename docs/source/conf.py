6ca9390ac5e2e6c679638a496977718c1731cc04
Nikolay Shuyskiy <klas@genestack.com>
2020-06-10 22:29:24 +0300

    py3: update rST docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,7 +38,7 @@ sys.path.insert(0, os.path.abspath('../../'))  # Use Python Client Library from 
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx']
 
-intersphinx_mapping = {'python': ('http://docs.python.org/2.7', None)}
+intersphinx_mapping = {'python': ('http://docs.python.org/3', None)}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
