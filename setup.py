fe3b6f16a46e9eab69299349cd772ded13e23f5d
Nikolay Shuyskiy <klas@genestack.com>
2020-11-14 17:06:41 +0300

    Promise Python 3.9 support

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         'Topic :: Software Development',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
         'Topic :: Software Development :: Libraries :: Python Modules',
