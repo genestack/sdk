1534ce1fe9dc1c7c59dba818d291051b01bbc583
Nikola Zorkic <nikola.zorkic@genestack.com>
2022-11-14 12:10:29 +0100

    ODM-8670: Remove leftover default_host

--- genestack_client/settings/__init__.py
+++ genestack_client/settings/__init__.py
@@ -8,6 +8,6 @@ from future import standard_library
 standard_library.install_aliases()
 from builtins import *
 from genestack_client.settings.genestack_config import config
-from genestack_client.settings.genestack_user import DEFAULT_HOST, User
+from genestack_client.settings.genestack_user import User
 
 
