ebfe7f8c7938a03802385d114414ddcba2696833
Kunitsyn Oleg <oleg.kunitsyn@genestack.com>
2023-04-08 21:43:20 +0200

    fix include

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
     long_description=__doc__,
     long_description_content_type="text/markdown",
     python_requires='>2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+    include=["requirements.txt"],
     install_requires=required,
     entry_points={
         'console_scripts': [
