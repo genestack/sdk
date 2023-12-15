a3c112ec5dd6f17feaafd5ec0af3b0d372a27df2
Mikhail Smazhevsky <mikhail.smazhevsky@genestack.com>
2023-12-15 20:22:58 +0100

    remove copyright

--- genestack_client/settings/genestack_user.py
+++ genestack_client/settings/genestack_user.py
@@ -1,11 +1,5 @@
 # -*- coding: utf-8 -*-
 
-#  Copyright (c) 2011-2023 Genestack Limited
-#  All Rights Reserved
-#  THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF GENESTACK LIMITED
-#  The copyright notice above does not evidence any
-#  actual or intended publication of such source code.
-
 from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division
