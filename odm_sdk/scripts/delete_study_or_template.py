a576739f989ab8ce9dcd6586576dd012a89c485a
Mikhail Smazhevsky <mikhail.smazhevsky@genestack.com>
2025-03-05 18:01:36 +0100

    ODM-12424: handling exceptions in delete_study_or_template.py script in accordance with server response

--- odm_sdk/scripts/delete_study_or_template.py
+++ odm_sdk/scripts/delete_study_or_template.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+#  Copyright (c) 2011-2025 Genestack Limited
+#  All Rights Reserved
+#  THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF GENESTACK LIMITED
+#  The copyright notice above does not evidence any
+#  actual or intended publication of such source code.
+
 # This script deletes files using wipeStudy method.
 # Provide an accession of a study/template which needs to be deleted.
 
@@ -27,13 +33,13 @@ def main():
         connection.application('genestack/study-metainfo-editor').invoke('wipeStudy', accession)
         print(colored("Success", GREEN))
     except GenestackServerException as e:
-        p = re.compile('File .* not found')
-        result = p.search(e.message)
-        if result is not None:
-            print(colored("Study/template with accession %s does not exist" % accession,
-                          RED))
-        else:
-            print(colored(e, RED))
+        message = e
+        if e.stack_trace is not None:
+            p = re.compile(r"GenestackRestApiException: (.*?)(?=\n)")
+            result = p.search(e.stack_trace)
+            if result:
+                message = result.group(1)
+        print(colored(message, RED))
 
 
 if __name__ == "__main__":
