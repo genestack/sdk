a9a552a951d93a5c97f5e197d0257e812c1739ae
George Nefedev <george.nefedev@genestack.com>
2025-03-19 09:54:12 +0100

    Substitute self-written health checks with SB-ones
    * remove copyright
    
    ODM-11981

--- odm_sdk/settings/user.py
+++ odm_sdk/settings/user.py
@@ -1,9 +1,3 @@
-#  Copyright (c) 2011-2025 Genestack Limited
-#  All Rights Reserved
-#  THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF GENESTACK LIMITED
-#  The copyright notice above does not evidence any
-#  actual or intended publication of such source code.
-
 from getpass import getpass
 from urllib.parse import urlsplit
 
