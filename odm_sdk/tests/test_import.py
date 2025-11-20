0765e92570a4cc6922e01e50d2ea4bb66c638130
Marijan Mihaljev <marijan.mihaljev@genestack.com>
2025-11-20 12:03:57 +0100

    ODM-12867: edit tests

--- odm_sdk/tests/test_import.py
+++ odm_sdk/tests/test_import.py
@@ -364,7 +364,7 @@ class ImportTest(unittest.TestCase):
                     "-e",
                     "https://expressions3",
                     "-pr",
-                    "https://preparations",
+                    "https://preparations1",
                     "-e",
                     "https://expressions4",
                     "-c",
@@ -378,14 +378,15 @@ class ImportTest(unittest.TestCase):
         sample_node_list = parsedArgs.sample_node_list
         self.assertEqual(2,
                          len(sample_node_list),
-                         "There should be two samples for the study")
+                         "There should be two samples for the study"
+                         " - https://samples1.tsv and https://samples2.tsv")
         get_cell_children = lambda parent: (
             [c for c in parent.get('children') if c['tag'] == 'cells']
         )
         active_sample = sample_node_list[0]
         self.assertEqual(1,
                          len(active_sample.get('children')),
-                         "First sample should have one child - preparations")
+                         "First sample should have one child - preparations (https://preparations)")
         preparation = active_sample.get('children')[0]
         self.assertEqual(2,
                          len(preparation.get('children')),
@@ -393,14 +394,16 @@ class ImportTest(unittest.TestCase):
         cell_children = get_cell_children(active_sample.get('children')[0])
         self.assertEqual(2,
                          len(cell_children),
-                         "First sample should have two cell children")
+                         "First sample's preparation should have two cell children"
+                         " - https://cell1 and https://cell2")
         self.assertEqual(1,
                          len(cell_children[1].get('children')),
-                         "Second cell should have one expression child")
+                         "Second cell should have one expression child (https://expressions1)")
         active_sample = sample_node_list[1]
         self.assertEqual(3,
                          len(active_sample.get('children')),
-                         "Second sample should have three children")
+                         "Second sample should have three children"
+                         " - https://cell3, https://library1 and https://preparations1")
         self.assertEqual(1,
                          len(get_cell_children(active_sample)),
                          "Second sample should have one cell child")
@@ -410,31 +413,31 @@ class ImportTest(unittest.TestCase):
                              "Second sample should have library")
         self.assertEqual(2,
                          len(library.get('children')),
-                         "Second sample's library should have three children")
+                         "Second sample's library should have two children"
+                         " - https://expressions2 and https://cell4")
         cell_children = get_cell_children(library)
         self.assertEqual(1,
                          len(cell_children),
                          "Second sample library should have one cell child")
         self.assertEqual(1,
                          len(cell_children[0].get('children')),
-                         "Second sample library cell child should have one expression child")
-        self.assertEqual(1,
-                         len(get_cell_children(library)),
-                         "Second sample's library should have one cell child")
+                         "Second sample's library cell child should have one expression child")
         preparation = next((p for p in active_sample.get('children') if p['tag'] == 'preparations'),
                            None)
         self.assertIsNotNone(preparation,
                              "Second sample should have one preparation")
         self.assertEqual(2,
                          len(preparation.get('children')),
-                         "Second sample's  preparation should have three children")
+                         "Second sample's preparation should have two children"
+                         " - https://expressions4 and https://cell5")
         cell_children = get_cell_children(preparation)
         self.assertEqual(1,
                          len(cell_children),
                          "Second sample's  preparation should have one cell child")
         self.assertEqual(2,
                          len(cell_children[0].get('children')),
-                         "Second sample's preparation cell should have two children")
+                         "Second sample's preparation cell should have two children"
+                         " - https://expressions5 and https://expressions6")
 
     def test_ImportParams_ODM_cell_expression(self):
         parser_args_state = ParserAstState()
@@ -443,7 +446,7 @@ class ImportTest(unittest.TestCase):
             '-t', 'tknRoot',
             '--host', 'https://test.io',
             '-sa', 'GSF000001',
-            #SAMPLE (1 explicit, 1 regular, 1 LIB, 1 PREP)
+            #SAMPLE (1 implicit cell, 1 regular cell, 1 LIB, 1 PREP)
             '-sm', 'GSF000002',
             '-c', 'GSF000003',
             '-e', 'https://testexp01.io',
@@ -455,19 +458,20 @@ class ImportTest(unittest.TestCase):
             '-c', 'https://testcell01.io',
             '-e', 'https://testexp03.io',
             '-nfa', '2',
-            #LIBRARY (1 explicit, 2 regular)
+            #LIBRARY (1 implicit cell, 2 regular cell)
             '-lb', 'https://testlib01.io',
             '-c', 'https://testcell02.io',
             '-e', 'https://testexp04.io',
             '-nfa', '2',
             '-dc', 'other',
+            '-em', 'https://expressionMetadata01.io',
             '-c', 'GSF000004',
             '-e', 'https://testexp05.io',
             '-nfa', '2',
             '-c', 'https://testcell03.io',
             '-e', 'https://testexp05.io',
             '-nfa', '2',
-            #PREPARATION (1 explicit, 1 regular)
+            #PREPARATION (1 implicit cell, 1 regular cell)
             '--preparations', 'https://testprep01.io',
             '-c', 'GSF000006',
             '-e', 'https://testexp06.io',
@@ -481,17 +485,18 @@ class ImportTest(unittest.TestCase):
         sample_node_list = parsedArgs.sample_node_list
         self.assertEqual(4,
                          len(sample_node_list),
-                         "There should be five samples nodes for the study")
+                         "There should be four samples nodes for the study"
+                         " - one regular (GSF000001) and three implicit")
         implicit_sample_nodes = [s for s in sample_node_list if s['value'] == 'implicit']
         self.assertEqual(3,
                          len(implicit_sample_nodes),
-                         "There should be three implicit samples nodes for the study, "
-                         "for cells accession GSF000003, GSF000004, GSF000006")
+                         "There should be three implicit samples nodes for the study,"
+                         " for cells accessions GSF000003, GSF000004, GSF000006")
         sample_node = sample_node_list[0]
         self.assertEqual(3, len(sample_node.get('children')),
                          "Sample node should have three children - "
-                         " cell (https://testcell01), "
-                         "library and preparation")
+                         " cell (https://testcell01),"
+                         " library and preparation")
         sample_cell = sample_node.get('children')[0]
         self.assertEqual(1, len(sample_cell.get('children')))
         self.assertEqual("https://testexp03.io", sample_cell.get('children')[0]['value'])
